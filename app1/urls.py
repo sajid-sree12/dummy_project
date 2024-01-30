6aff2eda27edca0f188bfae223c4e82100965778
Sajid <dsajid942@gmail.com>
2024-01-30 13:05:09 +0530

    uploading project

---
+++ app1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path("call1/support1",views.home,name="home"),
+    path("call2",views.home2,name="home2"),
+    path("call3",views.home3,name="home3"),
+    path("wish/<str:name>",views.greet,name="greet_name"),
+    path("<int:old>",views.greet2,name="greet2")
+]
---
+++ app1/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    
+    out_data={
+            "names":["sai","jay","shiva"],
+            "age":[25,40,67],
+            }
+    print(type(request))
+    return render(request,"index.html",out_data)
+
+def home2(request):
+    va=["sai","jay","shiva"]
+    out_data={
+            "names":["sai","jay","shiva"],
+            "age":[25,40,67],
+            }
+    print(type(out_data['names']))
+    #print(type(request))
+    return render(request,"legend/home.html",out_data)
+
+def home3(request):
+    va=[{'name':'sai','age':24},{'name':'Jay','age':21},{'name':'Shiva','age':19},{'name':'Jayanth','age':17}]
+
+    return render(request,"legend/home.html",{'data':va,'Branch':'Information Technology'})
+
+def greet(request,name):
+    result="Hello "+ name +"! How are you dude !"
+    return render(request,"output.html",{'out':result})
+
+def greet2(request,old):
+    va=[{'name':'sai','age':24},{'name':'Jay','age':21},{'name':'Shiva','age':20},{'name':'Jayanth','age':17}]
+    li=[]
+    for item in va:
+        if item['age']==old:
+            li.append(item)
+    return render(request,"legend/home.html",{'data':li})
+
+
+
+#{"st1":"sai","st2":"Jay","st3":"Shiva"}
+
+
+"""class car():
+    def __init__(name,model):
+        self.car_name=name
+        self.car_model=model
+    
+
+obj1=car(name="tata",model=2023)
+obj2=car(name="tata",model=2023)
+
+QuerySet=[obj1,obj2]"""
